cb2ea75c37b18c772b44b39c839acf9b8392e9b8
DevSandeepX <sandeep87565590@gmail.com>
2025-03-21 21:11:44 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 4000
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin:'http://localhost:5173'
+    origin:'*'
 }))
 connectDb();
 app.get("/", (req, res) => {
